a7ff9a9fb37005d52835a1e6560d83e727d0c946
Student <Student@Train11-3-PC059.control.ad.sit.kmutt.ac.th>
2025-07-22 16:16:51 +0700

    add BasicP_class_2.py and BasicP_class_2_workshop.py

---
+++ BasicP_class._2.py
@@ -0,0 +1,44 @@
+# x = 10
+# print("ค่าตัวแปร x:", x)
+# y = 20
+# print("ค่าตัวแปร y:", y)
+# print("Type ของ x:", type(x))
+# print("Type ของ y:", type(y))
+# num = "1"
+# print("ค่าของ num: ", num)
+# print("Type ของ num: ", type(num))
+# print("หลังจากแปลแปลงค่า")
+# print("Type ของ num: ", type(num))
+# mod = 10 % 4
+# print(mod)
+# ten = 10
+# one = 1
+# print(ten != one)
+# T =True
+# F =False
+# print( not T or F)
+# num1 = int(input("กรอกเลข1: "))
+# num2 = int(input("กรอกเลข2: "))
+# print("num1 =", num1)
+# print("num2 =", num2)
+# print("type num =", type(num1))
+# print("type num =", type(num2))
+# print(num1 > num2)
+
+
+x = 8
+y = 4
+if x == y:
+    print("inside if:", "x == y")
+elif x > y:
+    print("inside elif:", "x > y")
+elif x < y:
+    print("inside elif:", "x < y")
+else:
+    print("inside else:", "x != y")
+print("outside if:", "staterpack Yai Mak")
+
+T = True
+F = False
+if T and F:
+    print()
